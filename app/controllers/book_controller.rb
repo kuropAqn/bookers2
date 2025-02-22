0634839d1dc2637a4665adbeebc87d0ace7833ae
kuropAqn <kuropan0819@gmail.com>
2025-02-22 06:20:54 +0000

    3

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -9,19 +9,24 @@ class BookController < ApplicationController
 
   def create
     @book = Book.new
+    @book.user_id = current_user.id
     @book.save
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
     @book = Book.find(params[:id])
     @book.edit
-    redirect_to 
+    redirect_to
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book = .destroy
+    redirect_to book_index_path
   end
 
 end
